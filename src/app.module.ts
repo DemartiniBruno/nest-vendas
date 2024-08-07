7b9218df470aad87042e3a6403503547ffb4636a
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-08-07 13:25:29 -0300

    feat: criando modulo product

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, ProductModule],
   controllers: [],
   providers: [],
 })
