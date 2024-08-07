0bd55cc7d341c302f10dfbcb76dc6180a303529c
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-08-07 09:18:57 -0300

    feat: DTO de criação user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MinLength, minLength } from "class-validator";
+
+export class CreateUserDto {
+    
+    @IsNotEmpty()
+    nome:string;
+
+    @IsEmail()
+    email:string;
+
+    @MinLength(6)
+    senha:string;
+}
