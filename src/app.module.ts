2dc47b406047da314aff3665d242d46bfac16057
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-08-07 14:36:29 -0300

    feat: criado resource dos pedido

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { PedidoModule } from './pedido/pedido.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, ProductModule],
+  imports: [DatabaseModule, UserModule, ProductModule, PedidoModule],
   controllers: [],
   providers: [],
 })
